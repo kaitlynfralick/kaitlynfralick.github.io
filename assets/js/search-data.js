2ae6c3f03fa7a10b930b7ef9c5c462e8f9dcda6f
kaitlynfralick <kaitlyn.fralick@gmail.com>
2025-08-16 15:00:46 +0000

    Deploying to gh-pages from @ kaitlynfralick/kaitlynfralick.github.io@42c2437c2df34654e6f2c8b4561985dddf8e330b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,8 +431,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-gave-a-paper-at-the-2024-victorian-studies-association-of-western-canada-vsawc-conference-victorian-nature-and-artiface",
+          title: 'Gave a paper at the 2024 Victorian Studies Association of Western Canada (VSAWC)...',
           description: "",
           section: "News",},{id: "news-gave-a-paper-at-the-ends-of-prosody-princeton-prosody-archive-princeton-university-with-denae-dyck-green-book",
           title: 'Gave a paper at The Ends of Prosody (Princeton Prosody Archive, Princeton University)...',
