5cb844b0bf50e5a8a60418ad5d4a010455808f77
kaitlynfralick <kaitlyn.fralick@gmail.com>
2026-09-18 00:08:23 +0000

    Deploying to gh-pages from @ kaitlynfralick/kaitlynfralick.github.io@902018851c6d4046fd776d177e0fc24346be2f56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This CV was last updated Summer 2026. For a complete version of my CV, please contact me at 17kf2@queensu.ca.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
